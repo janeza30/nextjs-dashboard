9e9f5515e984a87e3c5d59eb8532f958d1789185
janeza30 <julio_anez_ayala@outlook.com>
2026-02-04 20:26:57 -0400

    almost ready

---
+++ src/app/lib/actions.js
@@ -0,0 +1,31 @@
+"use server";
+import { signIn, signOut } from '../../../auth';
+import { AuthError } from 'next-auth';
+
+export async function authenticate(
+  prevState,
+  formData,
+) {
+  try {
+    await signIn('credentials', {
+      redirect: true,
+      redirectTo: '/ui/dashboard',
+      email: formData.get('email'),
+      password: formData.get('password'),
+    });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
+
+export async function logout() {
+  await signOut({ redirectTo: '/' });
+}
